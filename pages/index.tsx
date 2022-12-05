82a27565b92a9cb734750ed1c40f6df3b6e5bdd5
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2022-12-05 20:26:55 +0700

    fixed add tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+      <Head>
+        <title>E-voting App</title>
+        <meta name="description" content="Voting App No.1 di Indonesia" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex flex-col container mx-auto justify-center py-44 m-auto space-y-3">
+        <h1 className="text-center text-5xl font-bold">Let's Start Voting</h1>
+      </div>
     </div>
   );
 }
