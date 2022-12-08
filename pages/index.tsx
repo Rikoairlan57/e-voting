7a08c9a70a19b650709beb4236f83696b6f352a5
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2022-12-08 16:48:59 +0700

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,12 +11,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="flex flex-col container mx-auto justify-center py-44 m-auto space-y-3">
-        <h1 className="text-center text-5xl font-bold">
-          Let&apos;s Start Voting
-        </h1>
-        <div className="text-center">
+        <h1 className="text-center text-5xl font-bold">Ayo Mulai Voting</h1>
+        <div className="text-center ">
           <span className="text-lg bg-zinc-100 py-1 px-3 font-medium">
-            Website Number 1 in Indonesia
+            Web Voting No.1 Di Indonesia
           </span>
         </div>
         <Image
@@ -28,6 +26,20 @@ export default function Home() {
           objectPosition="center"
           objectFit="contain"
         />
+
+        <div className="flex flex-row items-center justify-center space-x-5">
+          <Link href="/vote/create" legacyBehavior>
+            <a className="bg-zinc-800 text-sm font-bold text-white w-40 text-center py-3 border-2 border-transparent hover:bg-zinc-200 hover:text-zinc-800">
+              Buat Vote Baru
+            </a>
+          </Link>
+          <span>atau</span>
+          <Link href="/participant" legacyBehavior>
+            <a className="bg-white text-sm font-bold text-zinc-800  border-zinc-800 border-2 w-40 text-center py-3 hover:bg-zinc-800 hover:text-white">
+              Ikutan Vote
+            </a>
+          </Link>
+        </div>
       </div>
       <div className="flex flex-row items-center justify-center space-x-5">
         <Link href="/vote/create" legacyBehavior>
