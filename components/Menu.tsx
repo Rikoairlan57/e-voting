0d4463d8518aa5fd172a0c776ec4f0eadfd58d4f
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2022-12-09 13:13:39 +0700

    sessison login logout anda sessionProvider fixed

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -4,14 +4,20 @@ import Link from "next/link";
 import Button from "./Button";
 
 const Menu = () => {
-  //   const { data: session } = useSession();
+  const { data: session } = useSession();
   return (
     <div className="flex justify-between py-3 container mx-auto">
       <Link href="/" legacyBehavior>
         <a className="font-bold text-xl">E-Voting</a>
       </Link>
-
-      <Button onClick={signIn} text="Login" />
+      {session ? (
+        <div>
+          <span>{session.user.name}</span>
+          <Button onClick={signOut} text="Logout" />
+        </div>
+      ) : (
+        <Button onClick={signIn} text="Login" />
+      )}
     </div>
   );
 };
